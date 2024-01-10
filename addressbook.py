43b2e8cd1dad6a0fd092085da52f554221301103
Olena-24 <143661285+Olena-24@users.noreply.github.com>
2024-01-10 12:28:17 +0900

    Update addressbook.py

--- addressbook.py
+++ addressbook.py
@@ -1,7 +1,6 @@
 from collections import UserDict
 from record import Record, Phone, Birthday
 from serialization import JsonSerializer, PickleSerializer
-import pickle
 
 class AddressBook(UserDict):
     def __init__(self, serializer=PickleSerializer()):
@@ -13,70 +12,38 @@ class AddressBook(UserDict):
         self.data[record.name.value] = record
 
     def find(self, name: str):
-        if name in self.data:
-            # print(self.data[name])
-            return self.data[name]
-        return None
+        return self.data.get(name, None)
 
-    def search(self, value: str):
+    def search(self, value: str) -> str:
         if len(value) < 3:
-            return '\033[91mYou need at least 3 letters to search by name or 3 didgit to search by phone number.\033[0m'
-        result = ''
+            return 'You need at least 3 letters to search by name or 3 digits to search by phone number.'
+        results = []
         for name, rec in self.data.items():
-            if value.lower() in name.lower():
-                result += f'{str(rec)}\n'
-            for item in rec.phones:
-                if value in item.value:
-                    result += f'{str(rec)}'
-        if len(result) != 0:
-            return result
-        else:
-            return None
+            if value.lower() in name.lower() or any(value in phone.value for phone in rec.phones):
+                results.append(str(rec))
+        return '\n'.join(results) if results else 'No matches found.'
 
-    def delete(self, name: str):
+    def delete(self, name: str) -> str:
         if name in self.data:
-            self.data.pop(name)
+            del self.data[name]
             return f'The contact {name} has been deleted.'
         else:
             return f'The contact {name} not found.'
 
-    def iterator(self, item_number):
+    def iterator(self, item_number: int):
         counter = 0
-        result = f'Contacts:\n'
-        print(result)
-        print(self.data)
+        result = 'Contacts:\n'
         for item, record in self.data.items():
             result += f'{item}: {str(record)}\n'
             counter += 1
-            print(counter)
             if counter >= item_number:
                 yield result
                 counter = 0
                 result = ''
-        print(result)
         yield result
 
-    def write_to_file(self):
-        with open(self.file, 'wb') as file:
-            pickle.dump(self.data, file)
-
-    def read_from_file(self):
-        with open(self.file, 'rb') as file:
-            self.data = pickle.load(file)
-        return self.data
-
-
-    def write_to_file(self):
-        with open(self.file, 'wb') as file:
-            pickle.dump(self.data, file)
-
-    def read_from_file(self):
-        with open(self.file, 'rb') as file:
-            self.data = pickle.load(file)
-        return self.data
-
-   def save(self):
-       self.serializer.serialize(self.data, self.file)
+    def save(self):
+        self.serializer.serialize(self.data, self.file)
 
     def load(self):
         self.data = self.serializer.deserialize(self.file)
