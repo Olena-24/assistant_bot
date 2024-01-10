dfdb7ff38fc5451703b079fcf9eee8fcd5d528ad
Olena-24 <143661285+Olena-24@users.noreply.github.com>
2024-01-10 12:32:16 +0900

    Update field.py

--- field.py
+++ field.py
@@ -23,20 +23,28 @@ class Address(Field):
     pass
 
 class Email(Field):
-    def __init__(self, value):
-        super().__init__(value)
+    EMAIL_PATTERN = re.compile(r"^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
 
     @Field.value.setter
     def value(self, value):
-        pattern = r"^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-        if not re.search(pattern, value):
+        if not Email.EMAIL_PATTERN.search(value):
             raise ValueError('Invalid email format.')
         self._value = value
 
 class Birthday(Field):
-    def __init__(self, value):
-        super().__init__(value)
+    @Field.value.setter
+    def value(self, value):
+        try:
+            self._value = datetime.strptime(value, '%Y.%m.%d').date()
+        except ValueError:
+            raise ValueError('Invalid date format. Correct format: YYYY.MM.DD')
 
+class Phone(Field):
+    @Field.value.setter
+    def value(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError('The phone number should be digits only and have 10 symbols.')
+        self._value = value
     @Field.value.setter
     def value(self, value):
         try:
