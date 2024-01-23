8860419c9154c586710dce07e0ee5bc2e107c9ce
Olena-24 <143661285+Olena-24@users.noreply.github.com>
2024-01-23 13:07:30 +0900

    Update field.py

--- field.py
+++ field.py
@@ -17,16 +17,20 @@ class Name(Field):
     pass
 
 class Address(Field):
-    pass
+    def __str__(self):
+        return self._value if self._value else 'None'
 
 class Email(Field):
-    @Field.value.setter
-    def value(self, value):
+    def __init__(self, value):
+        # Validate the email format during initialization
         pattern = r"^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-        if not re.search(pattern, value):
+        if not re.match(pattern, value):
             raise ValueError('Invalid email format.')
         self._value = value
 
+    def __str__(self):
+        return self._value if self._value else 'None'
+    
 class Birthday(Field):
     @Field.value.setter
     def value(self, value):
@@ -35,9 +39,13 @@ class Birthday(Field):
         except ValueError:
             raise ValueError('Invalid date format. Correct format: YYYY.MM.DD')
 
+    def __str__(self):
+        return str(self._value) if self._value else 'None'
+
 class Phone(Field):
     @Field.value.setter
     def value(self, value):
         if not re.fullmatch(r'\d{10}', value):
             raise ValueError('The phone number should be digits only and have 10 symbols.')
         self._value = value
+
